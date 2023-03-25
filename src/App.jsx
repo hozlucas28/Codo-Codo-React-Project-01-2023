47cded468819ba86bf9e38b9eecd496a6804cfa5
hozlucas28 <hozlucas28@gmail.com>
2023-03-25 00:52:33 -0300

    Create index files

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+
+import { TEXTS } from './constants/TEXTS.js'
+
+import { getRndElement } from './utils/getRndElement.js'
+
+import Header from './components/Header.jsx'
+import SubHeader from './components/SubHeader.jsx'
+import Options from './components/Options.jsx'
+import Instructions from './components/Instructions.jsx'
+import Footer from './components/Footer.jsx'
+
+const options = [TEXTS.options.rock.value, TEXTS.options.paper.value, TEXTS.options.scissors.value]
+
+function App() {
+	const [playerOpt, setPlayerOpt] = useState('null')
+	const machineOpt = playerOpt !== 'null' ? getRndElement(options) : null
+
+	console.log({ playerOpt, machineOpt })
+
+	return (
+		<>
+			<Header />
+			<SubHeader
+				playerOpt={playerOpt}
+				machineOpt={machineOpt}
+			></SubHeader>
+			<Options
+				playerOpt={playerOpt}
+				machineOpt={machineOpt}
+				setPlayerOpt={setPlayerOpt}
+			/>
+			<Instructions />
+			<Footer />
+		</>
+	)
+}
+
+export default App
