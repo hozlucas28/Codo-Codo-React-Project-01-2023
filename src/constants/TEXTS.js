be8900c4ae57f37a4e0b7942a02afd0a9dd9bbd5
hozlucas28 <hozlucas28@gmail.com>
2023-03-25 00:57:28 -0300

    Create constants folder
    It's for any constant useful for each part of the app.

---
+++ src/constants/TEXTS.js
@@ -0,0 +1,41 @@
+export const TEXTS = {
+	header: '- Rock • Paper • Scissors -',
+
+	subHeader: {
+		default: 'Select an option...',
+		playerWins: 'THE PLAYER WINS!',
+		machineWins: 'THE MACHINE WINS!',
+		draw: 'DRAW, NO ONE WON!',
+	},
+
+	form: {
+		options: {
+			rock: {
+				text: 'Rock',
+				value: 'rock',
+				altImg: 'Illustration of a rock',
+			},
+
+			paper: {
+				text: 'Paper',
+				value: 'paper',
+				altImg: 'Illustration of a paper',
+			},
+
+			scissors: {
+				text: 'Scissors',
+				value: 'scissors',
+				altImg: 'Illustration of a scissors',
+			},
+		},
+
+		restart: 'Restart',
+	},
+
+	instructions: 'Click on a card to select rock, paper, or scissors. Then, the machine will select a random one.',
+
+	footer: {
+		copyright: 'Copyright © 2023 Lucas Hoz',
+		sourceCode: '[ View Source Code ]',
+	},
+}
